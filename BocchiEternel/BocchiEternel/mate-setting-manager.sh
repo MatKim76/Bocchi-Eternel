0334f58da3fe567a3d73edbdfcec85455b654bb9
Matéo <129748653+MatKim76@users.noreply.github.com>
2024-09-05 09:48:46 +0200

    Add files via upload

---
+++ BocchiEternel/BocchiEternel/mate-setting-manager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# modifier selon besoin
+
+#a mettre dans la racine étudiant
+#chmod u+x mate-setting-manager.sh
+#modifier le test.sh avec l'id etudiant'
+
+#changer le chemin
+cd MesJeux/BocchiEternel
+
+while true; do
+    java BocchiEternel
+    echo "Le programme s'est arrêté. Relance dans 5 secondes..."
+    sleep 5
+done
